ee12b55578e36215a27db715e329ffa62b512733
Diego Sánchez <diegosandmg@gmail.com>
2025-04-25 16:38:09 -0400

    format with ruff

--- app/main.py
+++ app/main.py
@@ -1,17 +1,20 @@
 import logging
+
 import boto3
 
 # Initialize the logger
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
+
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 def lambda_handler(event, context):
-    logger.info(f'boto3 version: {boto3.__version__}')
-    for message in event['Records']:
+    logger.info(f"boto3 version: {boto3.__version__}")
+    for message in event["Records"]:
         process_message(message)
     print("done")
+
 
 def process_message(message):
     try:
@@ -20,4 +23,3 @@ def process_message(message):
     except Exception as err:
         print("An error occurred")
         raise err
-
